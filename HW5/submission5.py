2a2ae6f1e7776b3680e278963654d1ec437c3034
Mark Tuhacek <78545586+Mtuhacek@users.noreply.github.com>
2022-04-04 19:23:45 -0400

    Update submission5.py

--- HW5/submission5.py
+++ HW5/submission5.py
@@ -10,7 +10,18 @@ For example, given n = 3, a solution set is:
 Write a function named generateParenthesis that takes an integer as an input and returns a list of strings 
 as an output. Note that you can define a function inside a function if necessary.
 """
-
+def generateParenthesis(N):   
+    output = []
+    def backwards(parenthesis, opening, closing):
+        if len(parenthesis) == 2 * N:
+            output.append(parenthesis)
+            return
+        if opening < N:
+            backwards(parenthesis + '(', opening + 1, closing)
+        if closing < opening:
+            backwards(parenthesis + ')', opening, closing + 1)
+    backwards('', 0, 0)
+    return output
 
 
 """
@@ -28,3 +39,15 @@ Input: "race a car"
 Output: false
 Write a function named isPalindrome that takes a string as an input and returns a bool as an output.
 """
+import string
+def isPalindrome(stringInput):
+    if stringInput == '':
+        return True
+    else:
+        stringNew = stringInput.replace(' ', '')
+        stringNew = stringNew.lower()
+        stringNew = stringNew.translate(str.maketrans('','', string.punctuation))
+        if (stringNew == stringNew[::-1]):
+            return True
+        else:
+          return False
