7990318f15e54bb7a6faee70ed96a9fa0b357d91
Mark Tuhacek <78545586+Mtuhacek@users.noreply.github.com>
2022-04-22 20:28:07 -0400

    Add files via upload

---
+++ MIDTERM/midterm_2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  2 13:31:48 2022
+
+@author: marco
+"""
+
+numbers = dict()
+n = 6
+i = 2
+while i<=n:
+    numbers[i]=i**2
+    i+=2
+print(numbers)
